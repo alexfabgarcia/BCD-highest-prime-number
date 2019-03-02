8bfbf5d42c701ba351e8d38980138e2864d5989f
Alex Garcia <alex.fabiano@invillia.com>
2019-03-01 21:12:02 -0300

    BCD handling was finished.

--- bcd-highest-prime-number.X/bcd_highest_prime_number.asm
+++ bcd-highest-prime-number.X/bcd_highest_prime_number.asm
@@ -28,26 +28,35 @@
 ; sem Code protection
 	    __config _XT_OSC & _WDT_OFF & _PWRTE_OFF & _CP_OFF
 
-; Troca de banco de memória
-bank0	    macro ; Selecionar o banco 0 da memória
+; Macros
+; Selecionar o banco 0 da memória
+bank0	    macro
 	    bcf		    STATUS, RP0
 	    bcf		    STATUS, RP1
 	    endm
 
-bank1	    macro ; Selecionar o banco 1 da memória
+; Selecionar o banco 1 da memória
+bank1	    macro
 	    bsf		    STATUS, RP0
 	    bcf		    STATUS, RP1
 	    endm
 
+; Skip the next instruction if W was less than or equal to the value
+; it was subtracted from.  This assumes that the carry flag has been
+; preserved from the last SUBWF or SUBLW instruction.
+skip_wle    macro
+	    btfss	    STATUS, C;skip if no borrow occurred
+	    endm
+
 ; Registradores de uso geral
 	    cblock	    H'20'
-	    bcdCounter		; H'20' Registrador para contar BCDs informados
-	    digitoUnidade	; H'21' Registrador para dígito de unidade
-	    digitoDezena	; H'22' Registrador para dígito de dezena
-	    digitoCentena	; H'23' Registrador para dígito de centena
+	    bcdCounter		; Registrador para contar BCDs informados
+	    bcdAccumulator	; Registrador para acumular BCDs
 	    timerCount		; Registrador auxiliar para tratar temporização
 				; juntamente com interrupção do Timer0
-	    
+	    bcdAux		; Auxilia na busca por BCDs na tabela look-up
+	    iteracao		; Controle de iteração
+
 	    ; Registradores para troca de contexto com interrupção
 	    W_TEMP
 	    STATUS_TEMP
@@ -59,19 +68,29 @@ bank1	    macro ; Selecionar o banco 1 da memória
 	    goto	    inicio	; Vá para label que indica inicio
 
 ; Vetor de Interrupção
-	    org		    H'0004'	; Endereço do vetor de interrupção
-	    
+	    org		    H'0004'	    ; Endereço do vetor de interrupção
 	    call	    salvarContexto  ; Salva contexto para tratar ISR
 	    
-	    btfss	    PIR1,TMR1IF	    ;Ocorreu overflow no Timer1?
-	    goto	    exitISR	    ;Não, saia da interrupção
-	    
-	    bcf		    PIR1,TMR1IF	    ; Limpa flag de interrupção TRM0
+	    btfss	    PIR1,TMR1IF	    ; Ocorreu overflow no Timer1?
+	    goto	    exitISR	    ; Não, saia da interrupção
+	    bcf		    PIR1,TMR1IF	    ; Limpa flag de interrupção TMR1IF
+
 	    decfsz	    timerCount, F   ; Decrementa timerCount. É zero?
-	    goto	    exitISR	    ; Não, saisa da interrupção
-	    
+	    goto	    exitISR	    ; Não, sai da interrupção
+
 	    ; Trata ISR (Interrupt Service Routine)
+	    ;movfw	    PORTA	    ; Lê o conteúdo da porta A
+	    movlw	    B'00110010'	    ; Temporário, o correto é a linha acima
+	    andlw	    H'0F'	    ; Só interessam os últimos 4 dígitos
+	    movwf	    bcdAux	    ; Armazena o conteúdo lido em bcdAux
+	    sublw	    B'00001001'	    ; Subtrai 9 do valor lido
+	    skip_wle			    ; Verifica se W é menor ou igual a 9
+	    goto	    beforeExitISR   ; BCD inválido. Sai da interrupção
+	    goto	    tratarBCD	    ; Realiza o tratamento do BCD
+
+beforeExitISR:
 	    call	    clearTimerCount
+
 exitISR:
 	    call	    recuperarContexto	; Retorna contexto antes de ISR
 	    retfie			; Retorna da interrupção
@@ -92,14 +111,19 @@ inicio:
 	    movlw	    B'00110001'	    ; T1CON para interrupção via Timer1
 	    movwf	    T1CON	    ; com internal clock e prescaler 1:8
 	    
+	    call	    clearBCDCount	    
 	    call	    clearTimerCount
 
 loop:
-	    goto	    loop
-	    ;goto	    $	    ; Loop infinito (vai para posição atual $)
+	    goto	    $	    ; Loop infinito (vai para posição atual $)
+
+clearBCDCount:
+	    movlw	    D'2'	    ; Define W = 2 pois serão lidos 3
+	    movwf	    bcdCounter	    ; números BCD: unidade, dez. e cent.
+	    return
 
 clearTimerCount:
-	    movlw	    D'128'
+	    movlw	    D'10'
 	    movwf	    timerCount
 	    return
 
@@ -126,5 +150,130 @@ recuperarContexto:
 	    SWAPF    W_TEMP,F         ;Swap W_TEMP
 	    SWAPF    W_TEMP,W         ;Swap W_TEMP into W
 	    return
+
+
+tratarBCD:
+	    movfw	    bcdCounter	    ; Armazena contador de BCD
+	    sublw	    D'2'	    ; Substrai 2 de W
+	    btfsc	    STATUS,Z	    ; Resultado igual a zero?
+	    goto	    unidadeBCD	    ; Tratar unidade
+	    movfw	    bcdCounter
+	    sublw	    D'1'	    ; Substrai 1 de W
+	    btfsc	    STATUS,Z	    ; Resultado igual a zero?
+	    goto	    dezenaBCD	    ; Tratar dezena
+	    goto	    centenaBCD	    ; Tratar centena
+
+unidadeBCD:
+	    movfw	    bcdAux	    ; Retorna BCD para W
+	    movwf	    bcdAccumulator  ; Inicia BCD com unidade informada
+	    goto	    sairTratamentoBCD	; unidade BCD tratada, saindo...
+
+dezenaBCD:
+	    movfw	    bcdAux
+	    btfsc	    STATUS,Z		; Resultado igual a zero?
+	    goto	    sairTratamentoBCD	; dezena BCD tratada, saindo...
+	    movfw	    bcdAccumulator
+	    addlw	    D'10'
+	    movwf	    bcdAccumulator
+	    decf	    bcdAux,F
+	    goto	    dezenaBCD
+
+centenaBCD:
+	    movfw	    bcdAux
+	    btfsc	    STATUS,Z		; Resultado igual a zero?
+	    goto	    sairCentenaBCD	; dezena BCD tratada, saindo...
+	    movfw	    bcdAccumulator
+	    addlw	    D'100'
+	    btfsc	    STATUS,C
+	    goto	    overflowBCD		; Overflow centena. BCD > 255
+	    movwf	    bcdAccumulator
+	    decf	    bcdAux,F
+	    goto	    centenaBCD
+
+sairCentenaBCD:
+	    call	    clearBCDCount
+	    goto	    beforeExitISR
+
+overflowBCD:
+	    call	    clearBCDCount
+	    goto	    beforeExitISR   ; Prosseguir execução
+
+sairTratamentoBCD:
+	    decf	    bcdCounter,F    ; Decrementa o contador BCD
+	    goto	    beforeExitISR   ; Prosseguir execução
+
+buscarPrimo:
+	    ; Procurando número primo
+	    call	    lookupPrimos
+	    subwf	    bcdAux,w
+	    btfsc	    STATUS,Z
+	    goto	    mostrarPrimo    ; Valor é igual
+	    btfsc	    STATUS,C
+	    nop				    ; bcdAux é maior que W
+	    ; ...
+	    INCFSZ	    W
+	    goto	    beforeExitISR
+	    goto	    buscarPrimo	    ; W é maior
+
+mostrarPrimo:
+	    movfw	    bcdAux
+	    movwf	    PORTB
+
+lookupPrimos:
+	    addwf	PCL,F
+	    retlw       D'2'
+	    retlw       D'3'
+	    retlw       D'5'
+	    retlw       D'7'
+	    retlw       D'11'
+	    retlw       D'13'
+	    retlw       D'17'
+	    retlw       D'19'
+	    retlw       D'23'
+	    retlw       D'29'
+	    retlw       D'31'
+	    retlw       D'37'
+	    retlw       D'41'
+	    retlw       D'43'
+	    retlw       D'47'
+	    retlw       D'53'
+	    retlw       D'59'
+	    retlw       D'61'
+	    retlw       D'67'
+	    retlw       D'71'
+	    retlw       D'73'
+	    retlw       D'79'
+	    retlw       D'83'
+	    retlw       D'89'
+	    retlw       D'97'
+	    retlw       D'101'
+	    retlw       D'103'
+	    retlw       D'107'
+	    retlw       D'109'
+	    retlw       D'113'
+	    retlw       D'127'
+	    retlw       D'131'
+	    retlw       D'137'
+	    retlw       D'139'
+	    retlw       D'149'
+	    retlw       D'151'
+	    retlw       D'157'
+	    retlw       D'163'
+	    retlw       D'167'
+	    retlw       D'173'
+	    retlw       D'179'
+	    retlw       D'181'
+	    retlw       D'191'
+	    retlw       D'193'
+	    retlw       D'197'
+	    retlw       D'199'
+	    retlw       D'211'
+	    retlw       D'223'
+	    retlw       D'227'
+	    retlw       D'229'
+	    retlw       D'233'
+	    retlw       D'239'
+	    retlw       D'241'
+	    retlw       D'251'
 
 	    end
